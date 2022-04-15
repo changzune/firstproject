f812c082699bfbffccb1e5364c8a73c3a4010a30
zune <changzunelee@gmail.com>
2022-04-15 21:35:12 +0900

    Initial commit

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYoy(Model model){
+        model.addAttribute("username","zune");
+
+    return "greetings"; //tepletes/greetings.mutache -> 브라우저로 전송!
+}
+
+
+
+}
