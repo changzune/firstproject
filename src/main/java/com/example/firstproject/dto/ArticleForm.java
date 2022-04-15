d957c73c64df8dcca6c34abb7df254a716a98e4a
zune <changzunelee@gmail.com>
2022-04-16 00:41:19 +0900

    springboot

---
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -0,0 +1,41 @@
+package com.example.firstproject.dto;
+
+
+public class ArticleForm {
+
+
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
+
+
+
